0d38c1408c2fd56123885806d5c8cb685f77db08
redoa <redoa@DESKTOP-E8QBH7S>
2021-08-16 22:00:15 +0900

    bootstrap 추가

--- src/main/java/com/oasis/config/WebSecurityConfig.java
+++ src/main/java/com/oasis/config/WebSecurityConfig.java
@@ -6,7 +6,9 @@ import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDeniedException;
@@ -19,15 +21,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import com.oasis.common.service.LoginService;
 import com.oasis.common.service.UserService;
+import com.oasis.common.vo.UserVo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -45,7 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 2
 
 	@Override
 	public void configure(WebSecurity web) { // 4
-		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/3rd/**");
+		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/3rd/**", "/scss/**");
 	}
 
 	@Override
@@ -59,7 +70,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 2
 				// .antMatchers("/register**").anonymous()
 				// .antMatchers("/register/**").anonymous()
 				// error을 추가하면 통합에러처리
-				.antMatchers("/login", "/signup", "/sign", "/logout", "/denied").permitAll() // 누구나 접근 허용
+				.antMatchers("/login", "/signup", "/sign", "/logout", "/denied", "/user/check/**", "/users/**").permitAll() // 누구나 접근 허용
 				.antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
 				.antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
 				.anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
@@ -88,7 +99,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 2
 						String errormsg = null;
 
 						if (exception instanceof BadCredentialsException) {
-							loginFailureCount(username);
+							//loginFailureCount(username);
 							errormsg = "error.BadCredentials";
 						} else if (exception instanceof InternalAuthenticationServiceException) {
 							errormsg = "error.BadCredentials";
@@ -117,7 +128,49 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 2
 						 * userService.disabledUsername(username); }
 						 */
 					}
-				}).and().logout().invalidateHttpSession(true).deleteCookies("JSESSIONID") // 8
+				})
+				.successHandler(new AuthenticationSuccessHandler() {
+				    private RequestCache requestCache = new HttpSessionRequestCache();
+				    private RedirectStrategy redirectStratgy = new DefaultRedirectStrategy();
+				    
+				    @Override
+				    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+				            Authentication authentication) throws IOException, ServletException {
+				        
+				    	clearAuthenticationAttributes(request);
+				        resultRedirectStrategy(request, response, authentication);
+				        
+				    }
+				    
+				    //로그인 실패 에러 세션 지우기
+				    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+				        HttpSession session = request.getSession(false);
+				        if(session==null) return;
+				        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+				        
+				        String username = request.getParameter("username");
+				        UserVo user = userService.getUserById(username);
+				        
+				        log.info("user : " + user);
+				        log.info("getEmail : " + user.getEmail());
+				        session.setAttribute("user", user);
+				    }
+				    
+				    //로그인 성공시, 어떤 URL로 Redirect 할 지 결정
+				    protected void resultRedirectStrategy(HttpServletRequest request, HttpServletResponse response,
+				            Authentication authentication) throws IOException, ServletException {
+				        
+				        SavedRequest savedRequest = requestCache.getRequest(request, response);
+				        
+				        if(savedRequest!=null) {
+				            String targetUrl = savedRequest.getRedirectUrl();
+				            redirectStratgy.sendRedirect(request, response, targetUrl);
+				        } else {
+				            redirectStratgy.sendRedirect(request, response, "/");
+				        }
+				    }
+				})
+				.and().logout().invalidateHttpSession(true).deleteCookies("JSESSIONID") // 8
 				.logoutSuccessUrl("/login") // 로그아웃 성공시 리다이렉트 주소
 				.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
 				.exceptionHandling().authenticationEntryPoint(loginUrlAuthenticationEntryPoint()) // Here
@@ -141,6 +194,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 2
 					final AuthenticationException authException) throws IOException, ServletException {
 
 				String ajaxHeader = request.getHeader("X-Requested-With");
+				log.info("ajaxHeader >>>>>>>>>>>>>>>>>>>>>>>>>>>> " + ajaxHeader);
 				if (ajaxHeader != null && "XMLHttpRequest".equals(ajaxHeader)) {
 					// InsufficientAuthenticationException
 					response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
