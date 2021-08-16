a082235815661a7e8e5fbc2220fa8442de55ce23
redoa <redoa@DESKTOP-E8QBH7S>
2021-08-16 21:53:36 +0900

    세션처리 로직 추가(60분)

--- src/main/java/com/oasis/OasisApplication.java
+++ src/main/java/com/oasis/OasisApplication.java
@@ -1,18 +1,28 @@
 package com.oasis;
 
+import javax.servlet.http.HttpSessionListener;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import com.oasis.config.SessionListener;
 
 @SpringBootApplication
-public class OasisApplication extends SpringBootServletInitializer{
+public class OasisApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(OasisApplication.class);
 	}
-	
+
+	@Bean
+	public HttpSessionListener httpSessionListener() {
+		return new SessionListener();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(OasisApplication.class, args);
 	}
